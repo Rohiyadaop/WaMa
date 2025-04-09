28edf63b5b4bbcf1c8eba32e4dea1d108a8a33bc
Rohiyadaop <ry95325932@gmail.com>
2025-04-09 13:10:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from  './components/header';
-import Homepage from './components/homepage'
+import Homepage from './components/homepage';
+import Footer from './components/footer';
 function App() {
 
   return (
     <>
             <Header />
             <Homepage />
+            <Footer />
     </>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import '../style/footer.css'
 function footer(){
 
     return(<>
+      <div class="glow-line2"></div>
    <span>this is your footer  </span> 
     
     </>)
