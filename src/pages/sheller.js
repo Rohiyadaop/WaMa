ec461ea744b4fbf150a89bf2e3227d41c15d797e
Rohiyadaop <ry95325932@gmail.com>
2025-05-02 20:09:16 +0530

    update

--- src/pages/sheller.js
+++ src/pages/sheller.js
@@ -1,30 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import '../style/sheller.css';
+import axios from 'axios';
 
 const wasteCategories = [
   {
     id: 1,
     title: 'Organic/Biodegradable Waste',
     description: 'Food scraps, garden waste, paper, etc. Can decompose naturally and be composted.',
-    image: 'https://www.bing.com/images/search?view=detailV2&ccid=eVMa27X8&id=7614CF1B20F32FC3D80291F27D91995FFB1DF17C&thid=OIP.eVMa27X8xZjK3pscVOQc8gHaEK&mediaurl=https%3a%2f%2fwww.cheaperwaste.co.uk%2fwp-content%2fuploads%2f2020%2f02%2fWaste-Management-Cover-Photo.png&exph=1080&expw=1920&q=waste+url&simid=608047119686645080&FORM=IRPRST&ck=257A97B1E7D769E0AE224EA8A821EB68&selectedIndex=4&itb=0', 
+    image: 'https://www.cheaperwaste.co.uk/wp-content/uploads/2020/02/Waste-Management-Cover-Photo.png',
   },
   {
     id: 2,
     title: 'Inorganic/Non-Biodegradable Waste',
     description: 'Plastics, metals, glass, etc. Does not break down easily in nature.',
-    image: 'https://www.bing.com/images/search?view=detailV2&ccid=V11oPbch&id=227DA3B33F216F4627DA1C7EE4CDF45480E8B329&thid=OIP.V11oPbch4iOoJUVbnifSRwHaDt&mediaurl=https%3a%2f%2fwww.techyv.com%2fsites%2fdefault%2f2023%2f04%2fusers%2fRajen%2fPrevent-Contamination-of-the-Environment.png&exph=300&expw=600&q=waste+url&simid=607991044619918906&FORM=IRPRST&ck=274A10A782CB931F37F55F54E51DCEC7&selectedIndex=1&itb=0',
+    image: 'https://www.techyv.com/sites/default/files/Prevent-Contamination-of-the-Environment.png',
   },
   {
     id: 3,
     title: 'Hazardous Waste',
     description: 'Chemicals, batteries, medical waste, paints, etc. Harmful to humans, animals, and the environment if not handled properly.',
-    image: 'https://www.bing.com/images/search?view=detailV2&ccid=2mziTjyd&id=7B809A0B5687FB28ACD7493DE4C1970E9A536C6E&thid=OIP.2mziTjydlV4mb4rniMUKsgHaE8&mediaurl=https%3a%2f%2fwww.umweltbundesamt.de%2fsites%2fdefault%2ffiles%2fmedien%2f378%2fbilder%2fdeponie_airart_fotolia_33491092_m.jpg&exph=1126&expw=1688&q=waste+url&simid=607992230006573123&FORM=IRPRST&ck=655B4C75E59FBCA7CDD738573CD82060&selectedIndex=16&itb=0',
+    image: 'https://www.umweltbundesamt.de/sites/default/files/medien/378/bilder/deponie_airart_fotolia_33491092_m.jpg',
   },
   {
     id: 4,
     title: 'E-Waste (Electronic Waste)',
     description: 'Old phones, computers, appliances, etc. Requires special recycling methods due to toxic materials.',
-    image: 'https://www.bing.com/images/search?view=detailV2&ccid=6QKPRhtG&id=4A3E9E40C09E2DF0AB0159905D97B2F309FBE8F9&thid=OIP.6QKPRhtGcfMTm0PQ8yQnKAHaEL&mediaurl=https%3a%2f%2fwww.cosmoconsult.com%2f_next%2fimage%3furl%3dhttps%3a%252F%252Fbackend.cosmoconsult.com%252Ffileadmin%252F_processed_%252Fa%252Fa%252Fcsm_2.2.13_Waste_and_Recycling_2_e00df17210.jpg%26w%3d3840%26q%3d75&exph=1986&expw=3524&q=waste+url&simid=608000222886193855&FORM=IRPRST&ck=BBCF80EDF05AA6149CE2B46B76F0A726&selectedIndex=29&itb=0',
+    image: 'https://backend.cosmoconsult.com/fileadmin/_processed_/a/a/csm_2.2.13_Waste_and_Recycling_2_e00df17210.jpg',
   },
 ];
 
@@ -82,6 +83,27 @@ const Sheller = () => {
     setIsHistoryOpen(!isHistoryOpen);
   };
 
+  // Function to handle the upload of video and other data
+  const [video, setVideo] = useState(null);
+
+  const handleChange1 = (e) => {
+    setVideo(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('video', video);
+
+    try {
+      const res = await axios.post('http://localhost:8080/sheller', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="sheller-container">
       <header className="sheller-header">Waste Categories</header>
@@ -121,7 +143,7 @@ const Sheller = () => {
           <form onSubmit={handleSubmit} className="waste-form">
             <label>
               Upload Video:
-              <input type="file" name="video" accept="video/*" onChange={handleChange} required />
+              <input type="file" name="video" accept="video/*" onChange={handleChange1} required />
             </label>
             <label>
               Amount of Waste (in kg):
@@ -135,7 +157,7 @@ const Sheller = () => {
               Location:
               <input type="text" name="location" value={formData.location} onChange={handleChange} required />
             </label>
-            <button type="submit">Send for Verification</button>
+            <button type="submit" onClick={handleUpload}>Send for Verification</button>
           </form>
         </div>
       )}
